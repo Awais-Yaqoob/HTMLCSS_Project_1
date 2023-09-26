5c7424c482f34a080518fa61b764eab0a356a72c
Awais <awaisyaqoob350@gmail.com>
2023-09-26 21:03:56 +0500

    basic carousel structure

---
+++ script.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Automatically advance to the next slide every 3 seconds
+setInterval(nextSlide, 3000);
